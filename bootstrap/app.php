3274060c4d482d360d628c41e1206729ef173486
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 19:27:08 +0500

    Installed telescope

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Middleware\SetLocaleMiddleware;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\SetLocaleMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +21,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if (request()->is('api/*') && $e->getPrevious() instanceof ModelNotFoundException) {
+                $model = Str::afterLast($e->getPrevious()->getModel(), '\\');
+                return response()->json(['message' => $model . ' not found'], 404);
+            }
+
+            return response()->json(['message' => __('errors.404')], 404);
+        });
     })->create();
