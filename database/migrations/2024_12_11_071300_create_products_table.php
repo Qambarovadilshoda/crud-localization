e2d4886d9734958175256c659ef4f90e1b6c5caf
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 15:21:40 +0500

    I added columns

--- database/migrations/2024_12_11_071300_create_products_table.php
+++ database/migrations/2024_12_11_071300_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
