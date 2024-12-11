e700270e65442a343f15e37848b033ed0e116f6b
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 15:21:19 +0500

    I added fillable and relation method to model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model implements TranslatableContract
 {
-    //
+    use Translatable;
+    public $translatedAttributes = ['name', 'description'];
+    protected $fillable = [
+        'user_id',
+        'price'
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
