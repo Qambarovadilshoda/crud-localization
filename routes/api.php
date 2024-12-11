f4129c3f2916e79216d01326b5239668a2b5d8e5
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 14:02:43 +0500

    Added route for login

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('setLocale')->group(function (){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
