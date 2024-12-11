a1267624b14d5b5c2ef6b237b95f312db212d94f
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 16:36:03 +0500

    Added show update and destroy methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -33,33 +34,50 @@ class ProductController extends Controller
     }
     public function index()
     {
-        //
+        $products = Product::with('user')->paginate(10);
+        return $this->responsePagination($products, ProductResource::collection($products->load('translations')));
     }
 
     public function store(StoreProductRequest $request)
-{
-    $product = Product::create([
-        'user_id' => Auth::id(),
-        'price' => $request->price
-    ]);
-    $translations = $this->prepareTranslations($request->translations, ['name', 'description']);
-    $product->fill($translations);
-    $product->save();
-    return $product->load('translations');
-}
+    {
+        $product = Product::create([
+            'user_id' => Auth::id(),
+            'price' => $request->price,
+        ]);
+        $translations = $this->prepareTranslations($request->translations, ['name', 'description']);
+        $product->fill($translations);
+        $product->save();
+        return $this->success(new ProductResource($product->load('translations')), __('success.created'));
+    }
 
     public function show(string $id)
     {
-        //
+        $product = Product::with('user')->find($id);
+        if (!$product) {
+            return $this->error(__('error.no'), 404);
+        }
+        return $this->success(new ProductResource($product->load('translations')));
     }
-
-    public function update(Request $request, string $id)
+    public function update(UpdateProductRequest $request, string $id)
     {
-        //
+        $product = Product::find($id);
+        if (!$product) {
+            return $this->error(__('error.no'), 404);
+        }
+        $product->price = $request->price;
+        $translations = $this->prepareTranslations($request->translations, ['name', 'description']);
+        $product->fill($translations);
+        $product->save();
+        return $this->success(new ProductResource($product->load('translations')), __('success.updated'));
     }
-
     public function destroy(string $id)
     {
-        //
+        $product = Product::find($id);
+        if (!$product) {
+            return $this->error(__('error.no'), 404);
+        }
+        $product->deleteTranslations();
+        $product->delete();
+        return $this->success([],__('success.deleted'), 204);
     }
 }
