cbdd3fa2a826af94d481f146a6cc7a6fe2b33045
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 13:36:40 +0500

    I added register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request){
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        return $this->success([], __( 'success.created'), 201);
+    }
 }
