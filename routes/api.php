f84ff18e2a552926ddba45d6713f1085daf53bf9
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 14:31:33 +0500

    Added route for logout

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 Route::middleware('setLocale')->group(function (){
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::get('/user', [AuthController::class, 'getUser']);
+        Route::get('/logout', [AuthController::class, 'logout']);
+    });
 });
