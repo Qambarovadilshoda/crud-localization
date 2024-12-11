1e48680127149c24e73b2a0a91c74c30c53436a3
Dilshoda <qambarovadilshoda867@gmail.com>
2024-12-11 15:20:28 +0500

    I added store method and prepareTranslations methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,45 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreProductRequest;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    protected function prepareTranslations(array $translations, array $columns): array
+    {
+        $preparedTranslations = [];
+
+        foreach ($translations as $translation) {
+            foreach ($translation as $lang => $value) {
+                if (!isset($preparedTranslations[$lang])) {
+                    $preparedTranslations[$lang] = [];
+                }
+
+                foreach ($columns as $column) {
+                    if (isset($value[$column])) {
+                        $preparedTranslations[$lang][$column] = $value[$column];
+                    } else {
+                        info("{$column} not set for language: $lang");
+                    }
+                }
+            }
+        }
+
+        return $preparedTranslations;
+    }
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function store(StoreProductRequest $request)
+{
+    $product = Product::create([
+        'user_id' => Auth::id(),
+        'price' => $request->price
+    ]);
+    $translations = $this->prepareTranslations($request->translations, ['name', 'description']);
+    $product->fill($translations);
+    $product->save();
+    return $product->load('translations');
+}
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
